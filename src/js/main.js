1f1f57ea485c8ff964c7ccef4eed49c2e709f5c0
SonSuBin129 <ttokky0129@gmail.com>
2024-05-31 13:11:00 +0900

    feat: 사용자 인사멘트 추가

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const userName = localStorage.getItem("name");
+  const sayUserElement = document.querySelector("#sayUser");
+
+  if (userName && sayUserElement) {
+    sayUserElement.textContent = `Hello, ${userName}!`; // 사용자 환영 메시지 설정
+  }
+});
